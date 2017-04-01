65a1ae8d92552b1c119a17299bbf23cb02cd8dc9
Nyuol <s3243720@student.rmit.edu.au>
2017-04-01 20:43:56 +1100

    made some changes

---
+++ src/Participant.java
@@ -0,0 +1,52 @@
+/**
+ * Created by Nyuol on 1/04/2017.
+ */
+abstract public class Participant {
+    String name;
+    String particpntID;
+    int age;
+    int phoneNumber;
+
+    public Participant(String name, String particpntID, int age, int phoneNumber ) {
+        this.name = name;
+        this.particpntID = particpntID;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParticpntID() {
+        return particpntID;
+    }
+
+    public void setParticpntID(String particpntID) {
+        this.particpntID = particpntID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    }
+}
